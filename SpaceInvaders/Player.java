46f320628dc9fab61bc6977b4f610658701ca629
Timasch06 <126168499+Timasch06@users.noreply.github.com>
2023-02-23 14:17:11 +0100

    Add files via upload

--- SpaceInvaders/Player.java
+++ SpaceInvaders/Player.java
@@ -1,25 +1,36 @@
 import java.awt.Graphics;
+import java.applet.*;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
 
 public class Player implements Consts
 {
-    private int x;
+    private int x,y;
+    
+    static Image image;
 
     public Player(){
-        
+        try {
+        image = ImageIO.read (new File ("H:/Dokumente/Enterprise TopDown sniped.png"));
+    }    catch(Exception e) {
+    }
     }
  
     public void draw(Graphics g){
-        g.drawRect(100, 100, 150, 150);
+        g.fillRect(x, 200, 50, 50);
+        x = x + 1;
+        g.drawImage(image, 0, 0, null);
     }
-    
     public void collide(){
-        
+        //x = x - 1;
     }
     
     public void move(boolean dir){
         
     }
-    
+    //700x 400
    /* public int getX(){
         
     }
