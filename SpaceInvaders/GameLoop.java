10d8e404e3f8e6a11bb72357d3ee5296fcc4f9b5
Quentin Weber <77012644+QuentinLeeWeber@users.noreply.github.com>
2023-03-09 13:46:42 +0100

    Add files via upload

--- SpaceInvaders/GameLoop.java
+++ SpaceInvaders/GameLoop.java
@@ -8,17 +8,20 @@ import javax.swing.JRootPane;
 import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
 import java.io.File;          
-import javax.imageio.ImageIO;    
+import javax.imageio.ImageIO;  
+import java.awt.image.BufferedImage; 
 
     public class GameLoop extends JPanel implements Consts{
     
     private JFrame frame = new JFrame();
     public static Player player = new Player();
-    private ArrayList<Enemy> enemies = new ArrayList<Enemy>();
-    private ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
-    private ArrayList<Shield> shields = new ArrayList<Shield>();
+    private static ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+    private static ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+    private static ArrayList<Shield> shields = new ArrayList<Shield>();
     Shield testShield = new Shield(200, 400 );
-    private Image image2;
+    public static Image image2;
+    
+    public static BufferedImage projectileImage;
     public GameLoop(){
 
     }
@@ -35,10 +38,11 @@ import javax.imageio.ImageIO;
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             try{
                 image2 = ImageIO.read(new File("space.jpg"));
-       
+                projectileImage = ImageIO.read(new File("shot.jpg"));
             }catch(Exception e){
-        
+          
             }
+        newGame();
         while(true){
             frame.repaint();
             //System.out.println("yeet");
@@ -49,22 +53,24 @@ import javax.imageio.ImageIO;
         shields.clear();
         enemies.clear();
         projectiles.clear();
-        
+        projectiles.add(new Projectile(100, 100));
         shields.add(new Shield(200, 400));
     }
     
     @Override
     protected void paintComponent(Graphics g){
         g.drawImage (image2, 0, 0, null); 
-        g.setColor(new Color(255, 0, 0));
-        g.fillRect(0, 0, 100, 100);
         player.draw(g);
         testShield.draw(g);
+        System.out.println(projectiles.size());
         for(int i = 0; i < shields.size();i++){
+            System.out.println("pro3");
             shields.get(i).draw(g);
+            
         }
         for(int i = 0; i < projectiles.size();i++){
             projectiles.get(i).draw(g);
+            System.out.println("pro2");
         }
         for(int i = 0; i < enemies.size();i++){
             enemies.get(i).draw(g);
@@ -72,7 +78,7 @@ import javax.imageio.ImageIO;
     }
     
     public static void newShoot(int x, int y){
-        
+        projectiles.add(new Projectile(x, y));
     }
     
     public static void newEnemyShoot(int x, int y){
--- SpaceInvaders/Projectile.java
+++ SpaceInvaders/Projectile.java
@@ -2,15 +2,18 @@ import java.awt.Graphics;
 
 public class Projectile implements Consts{
 
+    int x = 0;
+    int y = 0;
     
-    
-    public Projectile()
+    public Projectile(int _x, int _y)
     {
-
+        x = _x;
+        y = _y;
     }
     
     public void draw(Graphics g){
-        
+        g.drawImage(GameLoop.image2, x, y, null);
+        System.out.println("hi");
     }
     
     public void collide(){
